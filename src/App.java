26443d1c178bb6503ff1f00cb522c3abdcc8b11c
santirodriguezz <ssaannttii.rodriguez.alfaro@gmail.com>
2026-05-07 19:30:28 +0200

    Ejercicio7

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Christian Hidalgo");
         // Santiago Rodriguez Alfaro
+
+        System.out.println("ejercicio 7");
     }
 }
